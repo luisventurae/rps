47cd80b4ac8715ff7a36fcaf3b18fb2068f38c5b
Luis Ventura <luisigsystem@gmail.com>
2019-12-28 02:30:37 -0500

    set username

--- app/views/Home.vue
+++ app/views/Home.vue
@@ -2,6 +2,15 @@
   <view class="container">
     <StatusBar :bg_color="bg_color" />
     <Header :title="title" />
+    <view class="input-container"> 
+        <text-input class="input" placeholder="Nombre de Usuario" v-model="username" />
+        <touchable-opacity class="add-btn" :on-press="setUsername" >
+            <text class="btn-text">SAVE</text>
+        </touchable-opacity>
+    </view>
+    <view class="sets-container">
+        <text class="set-text">{{ set.username }}</text>
+    </view>
   </view>
 </template>
 
@@ -17,7 +26,17 @@ export default {
   data: () => ({
     bg_color: '#f39c12',
     title: 'RPS',
+    username: null,
+    set: {
+        username: null,
+    },
   }),
+  methods: {
+      async setUsername() {
+          this.set.username = this.username
+          this.username = null
+      },
+  },
 }
 </script>
 
@@ -25,5 +44,38 @@ export default {
 .container {
   background-color: white;
   flex: 1;
+}
+.input-container {
+    flex-direction: row;
+    justify-content: center;
+    align-content: center;
+}
+.input {
+    flex: 1;
+    height: 35px;
+    border-color: gray;
+    font-size: 20px;
+    padding: 5px;
+    color: #646464;
+}
+.add-btn {
+    width: 100px;
+    height: 35px;
+    background-color: #f39c12;
+    padding: 15px;
+    justify-content: center;
+    align-content: center;
+
+}
+.btn-text {
+    font-size: 20px;
+    font-weight: 700;
+    color: #fff;
+}
+.set-text {
+    width: 100%;
+    font-size: 40px;
+    justify-content: center;
+    align-content: center;
 }
 </style>
